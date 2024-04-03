8bade43f1fb2b8af5817d27071dc41c658ec0e36
Roby <robysetiawan2409@gmail.com>
2024-04-03 16:22:06 +0700

    feat(method): handling order book

--- src/components/sections/ListBooksSection.tsx
+++ src/components/sections/ListBooksSection.tsx
@@ -10,6 +10,9 @@ import {
 import Text from "../atoms/Text";
 import Button from "../atoms/button";
 import { UserEntity } from "@/types/user_entity";
+import { deleteCookie, getCookie, setCookie } from "@/utils/cookie-handling";
+import { toast } from "react-toastify";
+import { useRouter } from "next/router";
 
 export interface BookProps {
   books: Books[] | null;
@@ -17,8 +20,51 @@ export interface BookProps {
 }
 
 export function LinkBooksSection({ books, user }: BookProps) {
+  const router = useRouter();
   const handleClick = async (bookId: number) => {
-    console.log(`userid: ${user.id} book id : ${bookId}`);
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_BACKEND}/order`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${getCookie("token")}`,
+          },
+          body: JSON.stringify({ userId: user.id, bookId }), // Assuming userId is 1 for example
+        }
+      );
+      const data = await response.json();
+      if (response.ok) {
+        const user = data as UserEntity;
+        setCookie("user", JSON.stringify(user));
+        toast.success(`Welcome bro ${user.name} your point is ${user.point}`, {
+          position: "top-left",
+          autoClose: 400, // Close after 2 seconds
+          onClose: () => {
+            router.push("/myorders");
+          },
+        });
+      } else if (response.status === 401) {
+        deleteCookie("user");
+        deleteCookie("token");
+        toast.error(data.message, {
+          position: "top-left",
+          autoClose: 400, // Close after 2 seconds
+          onClose: () => {
+            router.push("/");
+          },
+        });
+      } else {
+        toast.error(`Failed to create order ${data.message}`, {
+          position: "top-left",
+        });
+      }
+    } catch (error) {
+      toast.error(`something wrong ${error}`, {
+        position: "top-left",
+      });
+    }
   };
 
   return (
@@ -50,7 +96,12 @@ export function LinkBooksSection({ books, user }: BookProps) {
                 )}
               </CardBookDescription>
               <Text className="text-teal-400">Point: {book.point}</Text>
-              <Button onClick={() => handleClick(book.id)}>Order</Button>
+              <Button
+                className="bg-blue-500 hover:bg-blue-700"
+                onClick={() => handleClick(book.id)}
+              >
+                Order
+              </Button>
             </CardBookFooter>
           </CardBook>
         ))
