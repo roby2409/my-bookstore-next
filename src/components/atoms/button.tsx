4146c63723af006acdbd6bb88ad2ef086ae227dd
Roby <robysetiawan2409@gmail.com>
2024-04-03 16:23:08 +0700

    refactor(component): change color button from outside (reusable)

--- src/components/atoms/button.tsx
+++ src/components/atoms/button.tsx
@@ -9,7 +9,7 @@ interface ButtonProps {
 const Button: React.FC<ButtonProps> = ({ onClick, children, className }) => {
   return (
     <button
-      className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ${className}`}
+      className={` text-white font-bold py-2 px-4 rounded ${className}`}
       onClick={onClick}
     >
       {children}
