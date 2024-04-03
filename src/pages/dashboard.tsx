89ad8d22acf92a0f0a5f3254169c50f14970cda2
Roby <robysetiawan2409@gmail.com>
2024-04-03 09:19:43 +0700

    feat(module): initialize for dashboard page and my orders page

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,12 @@
+import Text from "@/components/atoms/Text";
+import { LayoutDashboard } from "../components/layouts/LayoutDashboard";
+
+export default function Dashboard() {
+  return (
+    <LayoutDashboard>
+      <div className="w-full bg-white">
+        <Text>fsdkajfks</Text>
+      </div>
+    </LayoutDashboard>
+  );
+}
